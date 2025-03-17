83fb2c194f014b870d3aa9ef8b109372a1961260
Bing <RWBYBing@gmail.com>
2025-03-17 18:41:35 +0800

    Add Basic Rasterizer

---
+++ include/core/pipeline/rasterizer.h
@@ -0,0 +1,43 @@
+#ifndef RASTERIZER_H
+#define RASTERIXER_H
+
+#include <memory>
+#include <vector>
+
+#include <core/buffer/framebuffer.h>
+#include <core/buffer/depthbuffer.h>
+#include <core/primitives/fragment.h>
+#include <core/primitives/triangle.h>
+
+namespace Core
+{
+    namespace Pipeline
+    {
+        class Rasterizer
+        {
+        public:
+            Rasterizer(int width, int height);
+            ~Rasterizer();
+
+            // Set Buffer
+            void SetFrameBuffer(std::shared_ptr<Core::Buffer::FrameBuffer> buffer);
+            void SetDepthBuffer(std::shared_ptr<Core::Buffer::DepthBuffer> buffer);
+
+            // Rasterization processing
+            std::vector<Core::Primitives::Fragment> RasterizeTriangle(const Core::Primitives::Triangle& tri) const;
+            bool DepthTest(const Core::Primitives::Fragment& frag) const;
+            void WriteToFrameBuffer(const Core::Primitives::Fragment& frag);
+
+        
+        private:
+            int screen_width;
+            int screen_height;
+            std::shared_ptr<Core::Buffer::FrameBuffer> frame_buffer;    // frame buffer
+            std::shared_ptr<Core::Buffer::DepthBuffer> depth_buffer;    // depth buffer
+
+            // bool InsideTriangle(const Core::Primitives::Triangle& tri, int x, int y);   // Determine if a fragment is inside a triangle
+        };
+    }
+}
+
+#endif // RASTERIZER_H
---
+++ src/core/pipeline/rasterizer.cpp
@@ -0,0 +1,80 @@
+#include <core/pipeline/rasterizer.h>
+
+#include <algorithm>
+
+using namespace Core::Pipeline;
+
+Rasterizer::Rasterizer(int width, int height)
+    : screen_width{width}
+    , screen_height{height}
+    , frame_buffer{nullptr}
+    , depth_buffer{nullptr}
+{
+
+}
+
+Rasterizer::~Rasterizer()
+{
+
+}
+
+void Rasterizer::SetFrameBuffer(std::shared_ptr<Core::Buffer::FrameBuffer> buffer)
+{
+    this->frame_buffer = buffer;
+}
+
+void Rasterizer::SetDepthBuffer(std::shared_ptr<Core::Buffer::DepthBuffer> buffer)
+{
+    this->depth_buffer = buffer;
+}
+
+std::vector<Core::Primitives::Fragment> Rasterizer::RasterizeTriangle(const Core::Primitives::Triangle& tri) const
+{
+    std::vector<Core::Primitives::Fragment> fragments;
+
+    // Calculate the bounding box
+    double min_x = std::min({tri.vertices[0].pos.x, tri.vertices[1].pos.x, tri.vertices[2].pos.x});
+    double max_x = std::max({tri.vertices[0].pos.x, tri.vertices[1].pos.x, tri.vertices[2].pos.x});
+    double min_y = std::min({tri.vertices[0].pos.y, tri.vertices[1].pos.y, tri.vertices[2].pos.y});
+    double max_y = std::max({tri.vertices[0].pos.y, tri.vertices[1].pos.y, tri.vertices[2].pos.y});
+
+    // Iterate all the pixels in the bounding box
+    for (int y = static_cast<int>(min_y); y <= static_cast<int>(max_y); ++y)
+    {
+        for (int x = static_cast<int>(min_x); x <= static_cast<int>(max_x); ++x)
+        {
+            // TODO
+        }
+    }
+
+    return fragments;
+}
+
+bool Rasterizer::DepthTest(const Core::Primitives::Fragment& frag) const
+{
+    if (!this->depth_buffer)
+    {
+        return false;
+    }
+
+    double current_depth = this->depth_buffer->GetDepth(static_cast<int>(frag.pos.x), static_cast<int>(frag.pos.y));
+    if (frag.pos.z < current_depth)
+    {
+        this->depth_buffer->SetDepth(static_cast<int>(frag.pos.x), static_cast<int>(frag.pos.y), frag.pos.z);
+        return true;
+    }
+    return false;
+}
+
+void Rasterizer::WriteToFrameBuffer(const Core::Primitives::Fragment& frag)
+{
+    if (this->frame_buffer) 
+    {
+        this->frame_buffer->SetPixel(static_cast<int>(frag.pos.x), static_cast<int>(frag.pos.y), frag.color);
+    }
+}
+
+// bool Rasterizer::InsideTriangle(const Core::Primitives::Triangle& tri, int x, int y)
+// {
+//     return true;
+// }
